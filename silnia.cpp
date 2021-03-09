ba8d4c865e30f38351aba75311a7772fe473c50f
Gabriela Wielgus <gabwielgus@gmail.com>
2021-03-09 14:02:00 +0100

    Add new files

---
+++ silnia.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int silnia(int n)
+{
+	if (n <= 1) return 1;
+	if (n > 1) return n*silnia(n - 1);
+}
+int main()
+{
+	int n,d;
+	cin >> d;
+	if (d < 1 || d > 30) return 0;
+	for (int i = 0; i < d; i++)
+	{
+		cin >> n;
+		if (n < 0 || n> 1000000000) return 0;
+		else if (n <= 9)
+		{
+			int w = silnia(n);
+			cout << (w % 100) / 10 << " " << w % 10 << endl;
+		}
+		else if(n>=10)
+		{
+			cout << "0" << " " << "0" << endl;
+		}
+	}
+	return 0;
+}
